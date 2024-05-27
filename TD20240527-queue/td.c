56f774f554b10d8b8d7dfaa40c8ed40888fc27c4
pierrebressy <pierre.bressy@heig-vd.ch>
2024-05-27 13:37:55 +0000

    added queue

--- TD20240527-queue/td.c
+++ TD20240527-queue/td.c
@@ -3,17 +3,17 @@
 
 int main(int argc, const char *argv[])
 {
-	list stack;
+	list queue;
 	char *s = "Il fait beau et chaud";
 
-	init_queue(&stack);
+	init_queue(&queue);
 
 	for (int i = 0; i < strlen(s);i++) {
-		enqueue(&stack, s[i]);
+		enqueue(&queue, s[i]);
 	}
 
 	for (int i = 0; i < strlen(s);i++) {
-		element e = dequeue(&stack);
+		element e = dequeue(&queue);
 		printf("%c", e);
 	}
 	puts("");
